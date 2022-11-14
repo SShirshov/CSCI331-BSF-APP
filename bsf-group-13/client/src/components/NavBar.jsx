1d0f1d29fdbd077a5ab57a52e5a4687791f470cf
Bryce Leighton <bryce@brightvine.com>
2022-11-14 16:58:58 -0700

    amazing nav bar

---
+++ bsf-group-13/client/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav mr-auto h3">
+                <li className="nav-item">
+                    <a className="nav-link" href="">Ooga</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="">Booga</a>
+                </li>
+            </ul>
+        </nav>
+    )
+ }
