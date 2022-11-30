ab30a02dd58f1d80432157c71aba0afb9e860b5d
SShirshov <98186631+SShirshov@users.noreply.github.com>
2022-11-30 13:12:54 -0700

    update so runs

---
+++ bsf-group-13/server/DB-main/bridger.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 07, 2022 at 11:53 PM
+-- Server version: 5.7.34
+-- PHP Version: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bridger`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `ticketCode` varchar(8) NOT NULL,
+  `is_issued` tinyint(1) NOT NULL DEFAULT '0',
+  `issued_on` datetime NOT NULL,
+  `issued_to` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `volunteers`
+--
+
+CREATE TABLE `volunteers` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(16) DEFAULT NULL,
+  `last_name` varchar(32) DEFAULT NULL,
+  `email_address` varchar(32) DEFAULT NULL,
+  `time_in` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `volunteers`
+--
+
+INSERT INTO `volunteers` (`id`, `first_name`, `last_name`, `email_address`, `time_in`) VALUES
+(256, 'Keri', 'Hallau', 'keri.hallau@montana.edu', NULL),
+(257, 'Britney', 'Gibbs', 'brit@demo.com', NULL),
+(258, 'Daniel', 'DeFrance', 'dan@gmail.com', NULL),
+(271, 'kjsdf', 'asdf', 'sdfsdfsdf', NULL),
+(272, 'asdf', 'sdffffff', 'ffffff', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`ticketCode`),
+  ADD UNIQUE KEY `ticket` (`ticketCode`);
+
+--
+-- Indexes for table `volunteers`
+--
+ALTER TABLE `volunteers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `volunteers`
+--
+ALTER TABLE `volunteers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=273;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
