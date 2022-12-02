0f768e7064d1e4a5e8cd06b566697a75d54e7f9a
Bryce Leighton <bryce@brightvine.com>
2022-12-01 18:39:18 -0700

    messages page

--- bsf-group-13/client/src/components/App/App.js
+++ bsf-group-13/client/src/components/App/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import AddEntry from '../AddEntry.jsx';
-import CurrentEntries from '../CurrentEntries.jsx';
-import Footer from '../Footer.jsx'
 import {NavBar} from "../NavBar";
-import {BrowserRouter, Route, Router, Routes, useNavigate} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Entries from "../Entries";
 import ContactInfo from "../ContactInfo";
+import Messages from "./Messages";
 
 function App() {
 
@@ -17,6 +15,7 @@ function App() {
             <Routes>
                 <Route path={'/entries'} element={<Entries />}></Route>
                 <Route path={'/contactinfo'} element={<ContactInfo />}></Route>
+                <Route path={'/messages'} element={<Messages />}></Route>
             </Routes>
     </BrowserRouter>
       </div>
---
+++ bsf-group-13/client/src/components/App/Messages.jsx
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {DataGrid} from "@mui/x-data-grid";
+
+const Messages = () => {
+
+    const [messages, setMessages] = useState([])
+
+    // READ (GET)
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_HOST}/api/read_messages`).then((response) => {
+            setMessages(response.data)
+        })
+    }, [messages])
+
+    const columns = [
+        {
+            field: 'first_name',
+            headerName: 'Name',
+            //valueGetter: (params) => params.row.details.first_name,
+        },
+        {
+            field: 'email_address',
+            headerName: 'Email',
+            //valueGetter: (params) => params.row.details.email_address,
+        },
+
+        {
+            field: 'subjects',
+            headerName: 'Subject',
+            //valueGetter: (params) => params.row.details.subjects,
+        },
+
+        {
+            field: 'messagem',
+            headerName: 'Message',
+        },
+    ]
+
+    return (
+        <div>
+            <DataGrid
+                columns={columns}
+                rows={messages}
+            />
+        </div>
+    )
+}
+
+export default Messages;
